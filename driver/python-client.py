32451661c41a2ca5452341b8f4648d83b427cf45
pcrews <gleebix@gmail.com>
2013-02-20 09:10:35 -0800

    Modified python-client with temp debug output

--- driver/python-client.py
+++ driver/python-client.py
@@ -195,6 +195,9 @@ class lbaasDriver:
         if 'algorithm' in update_data:
             cmd += ' --algorithm=%s' %update_data['algorithm']
         status, output = self.execute_cmd(cmd)
+        print status
+        print output
+        print '@'*80
         data = output.split('\n')
         if output.strip() in ['',':']:
             status = '200'
