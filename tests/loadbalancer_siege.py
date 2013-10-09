2c2effed509cd375750b1621da702421406462c5
pcrews <gleebix@gmail.com>
2013-10-09 14:59:42 -0700

    Tweak of test and how we handle / track nodes being used vs. nodes available for use

--- tests/loadbalancer_siege.py
+++ tests/loadbalancer_siege.py
@@ -42,9 +42,10 @@ class testLoadBalancerSiege(unittest.TestCase):
             self.lb_name = str(lb_name)
         else:
             self.lb_name = lb_name
-        self.nodes = nodes
+        self.node_pool = nodes
         self.main_lb_id = None
-        self.original_nodes = None
+        # we have a pool of N nodes we can use, but set initial set to just 1 backend node
+        self.nodes = [self.node_pool[0]]
         self.lb_id = lb_id
         self.expected_status = expected_status
         self.first_run_done = False
@@ -75,10 +76,7 @@ class testLoadBalancerSiege(unittest.TestCase):
         """ test libra loadbalancers via siege
         """
         # Create our loadbalancer w/ one node to start
-        self.current_nodes = [self.nodes[0]]
-        self.logging.info("Current nodes: %s" %self.current_nodes)
-        self.logging.info("Nodes: %s" %self.nodes)
-        self.create_result, self.actual_status, self.lb_id, self.lb_addr = self.driver.create_lb(self.lb_name, self.current_nodes, self.algorithm, self.bad_statuses)
+        self.create_result, self.actual_status, self.lb_id, self.lb_addr = self.driver.create_lb(self.lb_name, self.nodes, self.algorithm, self.bad_statuses)
         self.logging.info('load balancer id: %s' %self.lb_id)
         self.logging.info('load balancer ip addr: %s' %self.lb_addr)
         lbaas_utils.validate_loadBalancer(self)
@@ -87,12 +85,12 @@ class testLoadBalancerSiege(unittest.TestCase):
         for node_count in [1,3,5,10]:
             if node_count != 1:
                 # we have nodes[0] already, we add subsequent nodes to it
-                add_nodes = self.nodes[len(self.current_nodes):node_count+1]
+                add_nodes = self.node_pool[len(self.nodes):node_count+1]
                 # add nodes to our loadbalancer
                 self.add_node_result, self.actual_status = self.driver.add_nodes(self.lb_id, add_nodes)
                 if self.actual_status =='202': 
                     # good update, we need to update our expected nodes
-                    self.current_nodes += add_nodes
+                    self.nodes += add_nodes
                     self.logging.info("Current node list:")
                     nodes = self.driver.list_lb_nodes(self.lb_id)
                     self.logging.info(nodes)
