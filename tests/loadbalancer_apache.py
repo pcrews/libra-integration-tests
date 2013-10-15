0949b8aa9eef3440ad25e22a66b552746488e293
pcrews <gleebix@gmail.com>
2013-10-15 14:08:04 -0700

    Tweak apache-bench and siege command lines for quicker runs

--- tests/loadbalancer_apache.py
+++ tests/loadbalancer_apache.py
@@ -122,7 +122,7 @@ class testLoadBalancerApache(unittest.TestCase):
                 page_path = os.path.join(self.lb_addr, page_file)
                 self.logging.info("Testing page: %s, %s" %(page_path, page_desc))
                 self.logging.info("Testing with %s nodes" %node_count)
-                cmd = 'ab -q -r -c500 -n10000 http://%s' %(page_path)
+                cmd = 'ab -q -r -c100 -n50000 http://%s' %(page_path)
                 self.logging.info("test command: %s" %cmd)
                 status, output = commands.getstatusoutput(cmd)
                 self.logging.info("status: %s" %status)
--- tests/loadbalancer_siege.py
+++ tests/loadbalancer_siege.py
@@ -122,7 +122,7 @@ class testLoadBalancerSiege(unittest.TestCase):
                 page_path = os.path.join(self.lb_addr, page_file)
                 self.logging.info("Testing page: %s, %s" %(page_path, page_desc))
                 self.logging.info("Testing with %s nodes" %node_count)
-                cmd = 'siege http://%s -d1 -r5000 -c200 -q' %(page_path)
+                cmd = 'siege http://%s -d1 -r100 -c100 -q' %(page_path)
                 self.logging.info("test command: %s" %cmd)
                 status, output = commands.getstatusoutput(cmd)
                 self.logging.info("status: %s" %status)
