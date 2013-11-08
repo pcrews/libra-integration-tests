c3ab626df51b0f560d021d8b70187dd7dd4b2c4b
pcrews <gleebix@gmail.com>
2013-11-08 12:08:17 -0800

    Tweaks to client driver

--- driver/python-client.py
+++ driver/python-client.py
@@ -192,10 +192,8 @@ class lbaasDriver:
         """ Remove specified node_id from lb_id """
         cmd = self.base_cmd + " node-delete --id=%s --nodeid=%s" %(lb_id, node_id)
         status, output = self.execute_cmd(cmd)
-        print cmd
-        print status
-        print output
-        print '@'*80
+        if status == 0:
+            return '202'
         return status
 
     def list_lbs(self):
