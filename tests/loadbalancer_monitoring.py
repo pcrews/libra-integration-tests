ca0f1be835f5128caef5660dee2e21c0a8a62dca
pcrews <gleebix@gmail.com>
2013-11-19 15:15:47 -0800

    Fix of typo

--- tests/loadbalancer_monitoring.py
+++ tests/loadbalancer_monitoring.py
@@ -162,7 +162,6 @@ class testMonitoring(unittest.TestCase):
         lbaas_utils.wait_for_active_status(self)
 
         self.logging.info("No timeout value")
-                    lbaas_utils.wait_for_active_status(self)
         monitor = {'delay': 100, 'attemptsBeforeDeactivation': 2, 'type': 'CONNECT'}
         output, status = self.driver.update_monitor(self.lb_id, monitor)
         self.logging.info(status)
