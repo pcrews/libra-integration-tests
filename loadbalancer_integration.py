b69e2ad334660adeb265e5a71e5b29affb4fd62a
pcrews <gleebix@gmail.com>
2013-12-02 10:05:53 -0800

    pep8 cleanup

--- loadbalancer_integration.py
+++ loadbalancer_integration.py
@@ -34,16 +34,17 @@ from lbaas_parser import get_parser
 
 #######
 # main
-#######  
+#######
 parser = get_parser()
 args = parser.parse_args(sys.argv[1:])
 if args.verbose:
     logging.info("VERBOSE: argument values:")
     for key, item in vars(args).items():
-        logging.info("VERBOSE: %s || %s" %(key, item))
+        logging.info("VERBOSE: %s || %s" % (key, item))
 
 # configure logging
-logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%Y%m%d-%H%M%S %p', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s %(message)s',
+                    datefmt='%Y%m%d-%H%M%S %p', level=logging.INFO)
 # disable INFO-level logging for the requests library...it is noise here
 requests_log = logging.getLogger("requests")
 requests_log.setLevel(logging.WARNING)
@@ -56,13 +57,17 @@ api_base_url = args.lbaasapiurl
 api_admin_url = None
 api_user_url = None
 if api_base_url:
-    api_admin_url = "%s:%s/%s" %(api_base_url, args.lbaasadminport, args.lbaasadminversion)
-    api_user_url = "%s:%s/%s" %(api_base_url, args.lbaasuserport, args.lbaasuserversion)
+    api_admin_url = "%s:%s/%s" % (api_base_url,
+                                  args.lbaasadminport,
+                                  args.lbaasadminversion)
+    api_user_url = "%s:%s/%s" % (api_base_url,
+                                 args.lbaasuserport,
+                                 args.lbaasuserversion)
 
 # load our specific driver
-driver_module = imp.load_source( args.driver
-                        , os.path.join(driver_path, args.driver+'.py'))
-driver = driver_module.lbaasDriver( args, api_user_url)
+driver_module = imp.load_source(args.driver,
+                                os.path.join(driver_path, args.driver + '.py'))
+driver = driver_module.lbaasDriver(args, api_user_url)
 
 ##################################
 # test away!
@@ -70,5 +75,3 @@ driver = driver_module.lbaasDriver( args, api_user_url)
 suite = load_lbaas_test_suite(args, args.variant_module, logging, driver)
 result = unittest.TextTestRunner(verbosity=2).run(suite)
 sys.exit(not result.wasSuccessful())
-
-
